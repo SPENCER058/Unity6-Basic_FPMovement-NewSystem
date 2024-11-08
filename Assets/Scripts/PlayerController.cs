ecd451017b517c9a57636e3ec89f86d80370e718
SPENCER058 <galihpanjidev@gmail.com>
2024-11-08 09:55:52 +0700

    Add Jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,7 +6,22 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private PlayerInput m_PlayerInput;
     [SerializeField] private PlayerMovement m_Movement;
 
+	private void OnEnable ()
+	{
+		m_PlayerInput.OnJumpInput += m_Movement.Jump;
+	}
+
 	private void Update ()
+	{
+		HandleMoveInput();
+	}
+
+	private void OnDisable ()
+	{
+		m_PlayerInput.OnJumpInput -= m_Movement.Jump;
+	}
+
+	private void HandleMoveInput ()
 	{
 		m_Movement.MovementDirection = m_PlayerInput.GetMovementInput();
 	}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,25 +2,85 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-	[Header("Stats")]
+	[Header("Movement Setting")]
 	[SerializeField] private float m_MoveSpeed = 5f;
 
-    [Header("Components")]
+	[Header("Movement Setting")]
+	[SerializeField] private float m_JumpHeight = 1.5f;
+	[SerializeField] private float m_MaxFallingSpeed = 2f;
+
+	[Header("World Behaviour")]
+	[SerializeField] private float m_Gravity = -9.8f;
+
+	[Header("Components")]
     [SerializeField] private CharacterController m_Controller;
 
 	public Vector2 MovementDirection { get; set; }
 
+	private bool m_IsGrounded;
+
+	private Vector3 m_PlayerVelocity;
+
+	#region Unity Life Cycles
+
+	private void Update ()
+	{
+		m_IsGrounded = m_Controller.isGrounded;
+		CalculateMoveDirection();
+	}
+
 	private void FixedUpdate ()
 	{
+		ApplyGravity();
 		MovePlayer();
 	}
 
-	private void MovePlayer ()
+	#endregion
+
+	#region Movement
+
+	private void CalculateMoveDirection ()
 	{
 		Vector3 newMoveDir = Vector3.zero;
 		newMoveDir.x = MovementDirection.x;
 		newMoveDir.z = MovementDirection.y;
 
-		m_Controller.Move(transform.TransformDirection(newMoveDir) * m_MoveSpeed * Time.fixedDeltaTime);
+		newMoveDir = transform.TransformDirection(newMoveDir) * m_MoveSpeed;
+
+		m_PlayerVelocity.x = newMoveDir.x;
+		m_PlayerVelocity.z = newMoveDir.z;
 	}
+
+	private void MovePlayer ()
+	{
+		m_Controller.Move(m_PlayerVelocity * Time.fixedDeltaTime);
+	}
+
+	#endregion
+
+	#region Jump and Gravity
+
+	private void ApplyGravity ()
+	{
+		// Gravity Accumulation
+		m_PlayerVelocity.y += m_Gravity * Time.fixedDeltaTime;
+
+		// Ground Check && Velocity Check
+		if (!m_IsGrounded && m_PlayerVelocity.y < -m_MaxFallingSpeed)
+		{
+			// Clamp velocity to prevent falling through the ground
+			m_PlayerVelocity.y = -m_MaxFallingSpeed;
+		}
+	}
+
+	public void Jump ()
+	{
+		if (m_IsGrounded)
+		{
+			// Standart Character Controller Jump By Unity Documentation
+			m_PlayerVelocity.y = Mathf.Sqrt(m_JumpHeight * -3.0f * m_Gravity);
+		}
+	}
+
+	#endregion
 }
